b341adc647720b2ce4431585d449e4c49b4b3a2b
Ravikumar Thota <suneetha.gopu3@gmailcom>
2023-11-08 16:17:53 -0500

    generate random images

--- script.js
+++ script.js
@@ -15,20 +15,38 @@ let secondcard = false;
 //Images array
 
 const images = [
-    {name : 'bee' , image = ""}
-    {name : 'bee' , image = ""}
-    {name : 'bee' , image = ""}
-    {name : 'bee' , image = ""}
-    {name : 'bee' , image = ""}
-    {name : 'bee' , image = ""}
-    {name : 'bee' , image = ""}
-    {name : 'bee' , image = ""}
-    {name : 'bee' , image = ""}
-    {name : 'bee' , image = ""}
-    {name : 'bee' , image = ""}
-    {name : 'bee' , image = ""}
-    {name : 'bee' , image = ""}
-    {name : 'bee' , image = ""}
-    {name : 'bee' , image = ""}
-    {name : 'bee' , image = ""}
+    {name : 'cat' , image: "cat.jpg"},
+    {name : 'tom' , image: "tom.jpg"},
+    {name : 'map' , image: "map.jpg"},
+    {name : 'rose' , image: "rose.jpg"},
+    {name : 'sun' , image: "sun.jpg"},
+    {name : 'dog' , image: "dog.jpg"},
+    {name : 'christmas' , image: "chrismas.jpg"},
+    {name : 'perscholas' , image: "perscholas"}
 ]
+
+let winCount = 0;
+    movesCount = 0;
+
+//Pick random Objects from the items array
+
+const generateRandom = (size = 4) => {
+
+    let tempArray = [...items];
+
+    let cardValues = [];
+
+    size = (size*size) / 2;
+
+    for(let i=0; i<=size; i++){
+        const randomIndex = Math.floor(MAth.random() * tempArray.length);
+
+        cardValues.push(tempArray[randomIndex]);
+
+        tempArray.splice(randomIndex, 1);
+    }
+ return cardValues;
+}
+
+
+
