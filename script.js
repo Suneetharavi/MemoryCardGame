ae147b6a87f9b992761ab770edb2a0c0f6c914e3
Ravikumar Thota <suneetha.gopu3@gmailcom>
2023-11-08 15:57:34 -0500

    added images

---
+++ script.js
@@ -0,0 +1,34 @@
+const moves = document.getElementById('moves-count');
+const timeValue = document.getElementById('time')
+const startButton = document.getElementById('start')
+const stopButton = document.getElementById('stop')
+const gameContainer = document.getElementById('.game-container')
+const result = document.getElementById('result')
+const controls = document.querySelector(controls-container);
+
+
+let cards;
+let interval;
+let firstCard = false;
+let secondcard = false;
+
+//Images array
+
+const images = [
+    {name : 'bee' , image = ""}
+    {name : 'bee' , image = ""}
+    {name : 'bee' , image = ""}
+    {name : 'bee' , image = ""}
+    {name : 'bee' , image = ""}
+    {name : 'bee' , image = ""}
+    {name : 'bee' , image = ""}
+    {name : 'bee' , image = ""}
+    {name : 'bee' , image = ""}
+    {name : 'bee' , image = ""}
+    {name : 'bee' , image = ""}
+    {name : 'bee' , image = ""}
+    {name : 'bee' , image = ""}
+    {name : 'bee' , image = ""}
+    {name : 'bee' , image = ""}
+    {name : 'bee' , image = ""}
+]
