9bf4021c1b3a6aa30df2f1e39e3cd85bac373671
Ravikumar Thota <suneetha.gopu3@gmailcom>
2023-11-08 18:38:28 -0500

    initializer

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const startButton = document.getElementById('start')
 const stopButton = document.getElementById('stop')
 const gameContainer = document.getElementById('.game-container')
 const result = document.getElementById('result')
-const controls = document.querySelector(controls-container);
+// const controls = document.querySelector(controls-container);
 
 
 let cards;
@@ -22,9 +22,9 @@ const images = [
     {name : 'sun' , image: "sun.jpg"},
     {name : 'dog' , image: "dog.jpg"},
     {name : 'christmas' , image: "chrismas.jpg"},
-    {name : 'perscholas' , image: "perscholas.jpg"}
-    {name : 'rainbow' , image: "rainbow.jpg"}
-    {name : 'nature' , image: "rainbows.jpg"}
+    {name : 'perscholas' , image: "perscholas.jpg"},
+    {name : 'rainbow' , image: "rainbow.jpg"},
+    {name : 'nature' , image: "rainbows.jpg"},
     {name : 'peacock' , image: "peacock.jpg"}
 ]
 
@@ -35,14 +35,14 @@ let winCount = 0;
 
 const generateRandom = (size = 4) => {
   //Creating a temporary Array
-    let tempArray = [...items];
+    let tempArray = [...images];
 //Initializes card values array
     let cardValues = [];
 //size should be double (4*4 matrix) /2 since pais of objects would exist.
     size = (size*size) / 2;
 //Random Object Selecton 
     for(let i=0; i<=size; i++){
-        const randomIndex = Math.floor(MAth.random() * tempArray.length);
+        const randomIndex = Math.floor(Math.random() * tempArray.length);
 
         cardValues.push(tempArray[randomIndex]);
 //Once selected remove the object from temp Array
@@ -52,13 +52,24 @@ const generateRandom = (size = 4) => {
 }
 
 const matrixGenerator = (cardValues, size = 4) => {
-    gameContainer.innerHTML = '';
+    gameContainer.innerHTML = "";
     cardValues = [...cardValues, ...cardValues]
     //shuffle
     cardValues.sort(() => Math.random() - 0.5);
     for(let i=0; i< size * size; i++){}
-
 }
+
+//Initialize values and func calls
+
+const initializer = () =>{
+    result.innerText = "";
+    winCount = 0;
+    let cardValues = generateRandom();
+    console.log(cardValues);
+    matrixGenerator(cardValues);
+};
+
+initializer();
 
 
 
