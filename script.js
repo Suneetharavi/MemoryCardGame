5707579483447bb010512fe65526b621ff3a7fe0
Ravikumar Thota <suneetha.gopu3@gmailcom>
2023-11-09 14:01:23 -0500

    css for 2 players

--- script.js
+++ script.js
@@ -133,6 +133,7 @@ const matrixGenerator = (cardValues, size = 4) => {
             if (winCount == Math.floor(cardValues.length / 2)) {
               result.innerHTML = `<h2>You Won</h2>
             <h4>Moves: ${movesCount}</h4>`;
+            // <h4>Time taken: ${minutesValue}:${secondsValue}</h4>`;
               stopGame();
             }
           } else {
